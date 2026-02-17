6d755bf685032a450cad95dc9c11ba3a7a1ef839
nonsecularman <krishbharti3404@gmail.com>
2026-02-17 21:27:16 +0530

    Update quotely.py

--- AloneMusic/plugins/tools/quotely.py
+++ AloneMusic/plugins/tools/quotely.py
@@ -1,6 +1,6 @@
 from pyrogram import filters
 from pyrogram.types import Message
-from SONALI import app
+from Alonemusic import app
 
 from PIL import Image, ImageDraw, ImageFont
 from io import BytesIO
